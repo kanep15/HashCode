edab57ac2e36db73c28d5f0f16c87f7da265ecb4
Alex <a.king.pro03@gmail.com>
2022-02-24 18:29:42 +0000

    People

--- People.java
+++ People.java
@@ -3,14 +3,10 @@ import java.util.Map;
 
 public class People {
     private String name;
-    private Map<String, Integer> skills = new HashMap<>();
+    private Map<String, Integer> skillLevels = new HashMap<>();
 
     public People(String name, Map map) {
-        skills = map;
+        skillLevels = map;
         name = name;
-    }
-
-    public void updateSkills() {
-
     }
 }
