1c90ba0cb851a5f71d228b1cf5d7b018d515f540
Alex <a.king.pro03@gmail.com>
2022-02-24 20:15:23 +0000

    Class finished

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 
     public static void main(String[] args){
-        //System.out.println("Hello World");
 
         ArrayList<Person> contributors = InputContributors.main();
         ArrayList<Project> projects = InputProjects.main();
@@ -12,9 +11,6 @@ public class Main {
         for(int i = 0; i< contributors.size(); i++){
             System.out.println(contributors.get(i).getName()+" "+contributors.get(i).getSkill());
             System.out.println(projects.get(i).getName()+" "+projects.get(i).getProjectSkill());
-
-
-
         }
     }
 
